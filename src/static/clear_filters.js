30200b48739726d0af10d8c5722f39e153a05363
TomPrint <tomekklewicki@gmail.com>
2023-01-29 15:40:21 +0100

    fitler search with HTMX

---
+++ src/static/clear_filters.js
@@ -0,0 +1,4 @@
+document.getElementById("clearButton").addEventListener("click", function() {
+    document.getElementById("searchInput").value = "";
+    document.getElementById("searchForm").submit();
+  });
