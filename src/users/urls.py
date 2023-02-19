d1ded358eb9b763968afbeb2b4dc81827c6d5a5e
TomPrint <tomekklewicki@gmail.com>
2023-02-19 09:40:10 +0100

    add initial setup for adminpage

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import adminpage
 
 
 urlpatterns = [ 
     path('login/', LoginView.as_view(template_name = 'users/login.html'), name= 'login'),
     path('logout/', LogoutView.as_view(template_name = 'users/logout.html'), name= 'logout'),
+    path('adminpage/', adminpage, name='adminpage'),
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+def adminpage(request):
+    users = User.objects.all()
+    context = {
+        
+        "title": "Admin Page",
+        "users": users,
+        
+    }
+    return render(request, "users/adminpage.html", context)
