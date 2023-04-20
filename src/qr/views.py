a4e4fd4937c2fea9f523f3ce7f63a35b11f8d593
TomPrint <tomekklewicki@gmail.com>
2023-04-20 23:49:56 +0200

    add unique suffix to pdf file name-update

--- src/qr/views.py
+++ src/qr/views.py
@@ -60,7 +60,8 @@ class Home(LoginRequiredMixin, ListView):
         context['object_list'] = queryset
         
         return context
-    
+
+
 @user_passes_test(lambda u: u.is_authenticated)
 def generate_qr(request, order_id):
     order = Order.objects.get(id=order_id)
@@ -91,47 +92,56 @@ def generate_qr(request, order_id):
     img.save(response, "PNG")
     return response
 
+#helper function for unique pdf file suffix
+def generate_random_suffix(length=3):
+    """Generate a random suffix of given length."""
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
 @user_passes_test(lambda u: u.is_authenticated)
 def add_order(request):
-    notifications= Notifications.objects.all()
+    notifications = Notifications.objects.all()
     email_list = [notification.email for notification in notifications]
     print(email_list)
     if request.method == 'POST':
         form = OrderForm(request.POST, request.FILES)
         if form.is_valid():
-            random_chars = ''.join(random.choices(string.ascii_letters + string.digits, k=3))
-
             order = form.save(commit=False)
             order.orderManager = request.user
+
+            # Generate random suffixes for each file
             if order.file:
-                order.file.name = order.file.name.split('.')[0] + '-' + random_chars + '.' + order.file.name.split('.')[-1]
+                order.file.name = order.file.name.split('.')[0] + '-' + generate_random_suffix() + '.' + order.file.name.split('.')[-1]
             if order.file2:
-                order.file2.name = order.file2.name.split('.')[0] + '-' + random_chars + '.' + order.file2.name.split('.')[-1]
+                order.file2.name = order.file2.name.split('.')[0] + '-' + generate_random_suffix() + '.' + order.file2.name.split('.')[-1]
             if order.file3:
-                order.file3.name = order.file3.name.split('.')[0] + '-' + random_chars + '.' + order.file3.name.split('.')[-1]
+                order.file3.name = order.file3.name.split('.')[0] + '-' + generate_random_suffix() + '.' + order.file3.name.split('.')[-1]
             if order.file4:
-                order.file4.name = order.file4.name.split('.')[0] + '-' + random_chars + '.' + order.file4.name.split('.')[-1]
+                order.file4.name = order.file4.name.split('.')[0] + '-' + generate_random_suffix() + '.' + order.file4.name.split('.')[-1]
+
             order.save()
             messages.success(request, "Dodano Zlecenie!")
-            
-            # sendgin email to user add in notifications app
+
+            # Sending email to user added in notifications app
             send_mail(
                 f"W Assemble QR dodano nowe zlecenie - {order.orderTag} !",
                 f' Zlecenie o numerze "{order.orderTag}" i nazwie "{order.orderName}", oczekuje na przygotowanie i dodanie instrukcji.',
                 "assembleqr@gmail.com",
-                email_list , fail_silently=True,
+                email_list, fail_silently=True,
             )
 
             return redirect('order_detail', order_uuid=order.url)
         else:
-            # clears fileLanguage fields on error
+            # Clears fileLanguage fields on error
             form = OrderForm(request.POST.copy())
             for key in ['fileLanguage', 'file2Language', 'file3Language', 'file4Language']:
                 form.data[key] = ''
-            messages.error(request, "Coś poszło nie tak! Pole języka musi być puste, jeśli nie wgrywasz pliku. Właściwy format to PDF do 2 MB. Nie może być 2 takich samych zleceń WZP.")
+            messages.error(request,
+                           "Coś poszło nie tak! Pole języka musi być puste, jeśli nie wgrywasz pliku. Właściwy format to PDF do 2 MB. Nie może być 2 takich samych zleceń WZP.")
     else:
         form = OrderForm()
     return render(request, 'qr/add_order.html', {'form': form})
+
 
 
 # deletes old file instance on update or delete
@@ -157,13 +167,13 @@ def update_order(request, order_uuid):
             random_chars = ''.join(random.choices(string.ascii_letters + string.digits, k=3))
 
             order = form.save(commit=False)
-            if order.file:
+            if 'file' in form.changed_data and order.file:
                 order.file.name = order.file.name.split('.')[0] + '-' + random_chars + '.' + order.file.name.split('.')[-1]
-            if order.file2:
+            if 'file2' in form.changed_data and order.file2:
                 order.file2.name = order.file2.name.split('.')[0] + '-' + random_chars + '.' + order.file2.name.split('.')[-1]
-            if order.file3:
+            if 'file3' in form.changed_data and order.file3:
                 order.file3.name = order.file3.name.split('.')[0] + '-' + random_chars + '.' + order.file3.name.split('.')[-1]
-            if order.file4:
+            if 'file4' in form.changed_data and order.file4:
                 order.file4.name = order.file4.name.split('.')[0] + '-' + random_chars + '.' + order.file4.name.split('.')[-1]
             order.save()
             messages.success(request, "Zaktualizowano Zlecenie!")
